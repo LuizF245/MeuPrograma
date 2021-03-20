20742d02e98ab9f972186d2311638330b2bb7fdc
Luiz Fernando A. M <0000109797831XSP@al.educacao.sp.gov.br>
2021-03-20 13:35:28 -0300

    Olá mundo

---
+++ src/io/github/luizf/MeuPrograma.java
@@ -0,0 +1,8 @@
+package io.github.luizf;
+
+public class MeuPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Olá mundo! Minha primeira aplicação Java!!");
+    }
+}
